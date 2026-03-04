91edf480fab0fdf356dc83f9d8fa266ca351a995
c0ngnh <96647368+c0ngnh@users.noreply.github.com>
2026-03-04 11:59:11 +0700

    Center message for power unplugging notification
    Replaced static text with centered message for unplugging power.

--- install_button.sh
+++ install_button.sh
@@ -119,7 +119,12 @@ def main():
         font_sm = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 14)
 
         draw.text((25, 30), "SYSTEM OFFLINE", font=font_lg, fill=0)
-        draw.text((20, 65), "It is safe to unplug power in next 10 seconds...", font=font_sm, fill=0)
+        
+        # Auto-calculate the exact center for the long middle line
+        msg = "You can unplug power in next 10 secs!"
+        msg_width = draw.textlength(msg, font=font_sm)
+        x_center = (image.width - msg_width) / 2
+        draw.text((x_center, 65), msg, font=font_sm, fill=0)
         draw.text((95, 95), "Z z z . . .", font=font_sm, fill=0)
 
         epd.display(epd.getbuffer(image))
